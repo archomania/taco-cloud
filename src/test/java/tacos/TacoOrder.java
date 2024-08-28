8d101fab84df60260165a76b6d7e3a4c5b297107
archomania <archomania@gmail.com>
2024-08-27 21:01:06 -0400

    Initial commit

---
+++ src/test/java/tacos/TacoOrder.java
@@ -0,0 +1,24 @@
+package tacos;
+import java.util.List;
+import java.util.ArrayList;
+import lombok.Data;
+
+@Data
+public class TacoOrder {
+    
+    private String deliveryName;
+    private String diliverStreet;
+    private String deliveryCity;
+    private String deliveryState;
+    private String deliveryZip;
+    private String ccNumber;
+    private String ccExpiration;
+    private String ccCvv;
+
+    private List<Taco> tacos = new ArrayList();
+
+    public void addTaco(Taco taco) {
+        tacos.add(taco);
+    }
+
+}
